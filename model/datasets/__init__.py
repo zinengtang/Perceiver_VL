b8854dbb373ef6f7b613ed7602863af9b158e36c
Zineng Tang <82334223+zinengtang@users.noreply.github.com>
2022-03-13 22:50:11 -0400

    Update __init__.py

--- model/datasets/__init__.py
+++ model/datasets/__init__.py
@@ -1,12 +1,7 @@
-from .vg_caption_dataset import VisualGenomeCaptionDataset
-from .coco_caption_karpathy_dataset import CocoCaptionKarpathyDataset
-from .f30k_caption_karpathy_dataset import F30KCaptionKarpathyDataset
+
 from .conceptual_caption_dataset import ConceptualCaptionDataset
-from .sbu_caption_dataset import SBUCaptionDataset
 from .vqav2_dataset import VQAv2Dataset
-from .nlvr2_dataset import NLVR2Dataset
 from .imagenet_dataset import ImagenetDataset
-from .imagenet1k_dataset import Imagenet1kDataset
 from .webvid_dataset import WebvidDataset
 from .msrvtt_dataset import MsrvttDataset
 from .base_video_dataset import BaseVideoDataset

72d4ed686a1d1a3bd3f413fe252322b4a60a79b0
Zineng Tang <82334223+zinengtang@users.noreply.github.com>
2022-03-13 22:46:08 -0400

    Update msrvtt_dataset.py

--- model/datasets/msrvtt_dataset.py
+++ model/datasets/msrvtt_dataset.py
@@ -53,28 +53,11 @@ class MsrvttDataset(BaseVideoDataset):
         split_dir = os.path.join(self.metadata_dir, 'high-quality', 'structured-symlinks')
         js_test_cap_idx_path = None
         challenge_splits = {"val", "public_server_val", "public_server_test"}
-#         if self.cut == "miech":
-#             train_list_path = "train_list_miech.txt"
-#             test_list_path = "test_list_miech.txt"
-#         elif self.cut == "jsfusion":
-#             train_list_path = "train_list_jsfusion.txt"
-#             test_list_path = "val_list_jsfusion.txt"
-#             js_test_cap_idx_path = "jsfusion_val_caption_idx.pkl"
-#         elif self.cut in {"full-val", "full-test"}:
         train_list_path = "train_list_full.txt"
         if self.cut == "full-val":
             test_list_path = "val_list_full.txt"
         else:
             test_list_path = "test_list_full.txt"
-#         elif self.cut in challenge_splits:
-#             train_list_path = "train_list.txt"
-#             if self.cut == "val":
-#                 test_list_path = f"{self.cut}_list.txt"
-#             else:
-#                 test_list_path = f"{self.cut}.txt"
-#         else:
-#             msg = "unrecognised MSRVTT split: {}"
-#             raise ValueError(msg.format(self.cut))
 
         train_df = pd.read_csv(os.path.join(split_dir, train_list_path), names=['videoid'])
         test_df = pd.read_csv(os.path.join(split_dir, test_list_path), names=['videoid'])
@@ -89,7 +72,7 @@ class MsrvttDataset(BaseVideoDataset):
         if self.subsample < 1:
             self.metadata = self.metadata.sample(frac=self.subsample)
 
-        # use specific caption idx's in jsfusion
+
         if js_test_cap_idx_path is not None and self.split != 'train':
             caps = pd.Series(np.load(os.path.join(split_dir, js_test_cap_idx_path), allow_pickle=True))
             new_res = pd.DataFrame({'caps': self.metadata, 'cap_idx': caps})

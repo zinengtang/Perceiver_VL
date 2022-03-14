e96a25a5aa381bbd5906f0fc33706a017f83e438
Zineng Tang <82334223+zinengtang@users.noreply.github.com>
2022-03-13 22:34:09 -0400

    Update co_pretrain.sh

--- scripts/co_pretrain.sh
+++ scripts/co_pretrain.sh
@@ -1,5 +1,5 @@
-python run.py with data_root='/playpen/terran/cc/' gpus=3 num_nodes=1 task_mlm_itm_vtm step200k \
-per_gpu_batchsize=3 model_type='PerceiverVL_version0' learning_rate=1e-5 \
-draw_false_video=1 draw_false_image=1 max_frames=1 latent_size_s=128 use_text=True use_video=True alternate_batch=True use_mpp=False use_decoder=True \
+python run.py with data_root='/playpen/terran/cc/' gpus=4 num_nodes=1 task_mlm_itm_vtm step200k \
+per_gpu_batchsize=3 model_type='PerceiverVL' learning_rate=1e-5 \
+draw_false_video=1 draw_false_image=1 max_frames=8 latent_size_s=128 use_text=True use_video=True alternate_batch=True use_mpp=False use_decoder=True \
 val_check_interval=0.05 num_workers=10 layer_drop=0.0 \
 load_path=""

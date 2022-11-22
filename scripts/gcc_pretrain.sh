b6620e2313e6180c22ba459dc5d083cd2109444b
Zineng Tang <82334223+zinengtang@users.noreply.github.com>
2022-11-21 22:32:29 -0500

    Update gcc_pretrain.sh

--- scripts/gcc_pretrain.sh
+++ scripts/gcc_pretrain.sh
@@ -1,5 +1,5 @@
 python run.py with data_root='datasets/cc/' gpus=4 num_nodes=1 task_mlm_itm step200k \
-per_gpu_batchsize=24 model_type='PerceiverVL' learning_rate=3e-4 \
+per_gpu_batchsize=16 model_type='PerceiverVL' learning_rate=3e-4 \
 draw_false_image=1 latent_size_s=128 use_text=True use_decoder=True use_video=False \
-val_check_interval=0.2 num_workers=16 layer_drop=0.5 use_mpp=False \
+val_check_interval=0.2 num_workers=12 layer_drop=0.5 use_mpp=False \
 load_path=""

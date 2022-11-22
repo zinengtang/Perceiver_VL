0729edb03fa9c65aa41567d3f09986aceac04b2c
Zineng Tang <82334223+zinengtang@users.noreply.github.com>
2022-11-21 22:32:38 -0500

    Update imagenet_pretrain.sh

--- scripts/imagenet_pretrain.sh
+++ scripts/imagenet_pretrain.sh
@@ -1,5 +1,5 @@
 python run.py with data_root='datasets/winter21_whole/' \
 gpus=4 num_nodes=1 task_cls_imagenet step200k \
 per_gpu_batchsize=16 learning_rate=8e-4 num_workers=12 val_check_interval=0.2 load_pretrain=False \
-model_type='PerceiverVL' latent_size_s=128 layer_drop=0.0 \
+model_type='PerceiverVL' latent_size_s=128 layer_drop=0.5 \
 load_path=""

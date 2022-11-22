c39d74b00ff95dd8e552aa38bc8332c4fbb7f477
Zineng Tang <82334223+zinengtang@users.noreply.github.com>
2022-11-21 22:31:17 -0500

    Update webvid_pretrain.sh

--- scripts/webvid_pretrain.sh
+++ scripts/webvid_pretrain.sh
@@ -1,4 +1,4 @@
 python run.py with data_root='datasets/' gpus=4 num_nodes=1 task_mlm_vtm_mpp step200k \
-per_gpu_batchsize=8 model_type='PerceiverVL' learning_rate=3e-4 \
+per_gpu_batchsize=16 model_type='PerceiverVL' learning_rate=1e-5 \
 draw_false_video=1 draw_false_image=1 max_frames=8 latent_size_s=128 latent_size_t=1 use_text=True use_decoder=True use_video=True \
 alternate_batch=False layer_drop=0.5 val_check_interval=0.2 num_workers=12 \

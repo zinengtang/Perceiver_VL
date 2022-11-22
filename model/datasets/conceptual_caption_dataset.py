9f0982797a58e6dbc972d22602a19100bc620cec
Zineng Tang <82334223+zinengtang@users.noreply.github.com>
2022-11-22 02:57:05 -0500

    Update conceptual_caption_dataset.py

--- model/datasets/conceptual_caption_dataset.py
+++ model/datasets/conceptual_caption_dataset.py
@@ -1,7 +1,8 @@
 from glob import glob
-from .base_image_dataset import BaseImageDataset
 import json
 from PIL import Image
+
+from .base_image_dataset import BaseImageDataset
 
 class ConceptualCaptionDataset(BaseImageDataset):
     def __init__(self, *args, split="", **kwargs):
@@ -10,14 +11,14 @@ class ConceptualCaptionDataset(BaseImageDataset):
         super().__init__(*args, **kwargs, )
         
     def _load_metadata(self):
-        self.folder = self.data_dir+'cc/'
+        self.data_dir = self.data_dir+'cc/'
         if self.split == 'train':
-            self.metadata = json.load(open(self.folder+'train_dict.json'))
+            self.metadata = json.load(open(self.data_dir+'train_dict.json'))
         else:
-            self.metadata = json.load(open(self.folder+'valid_dict.json'))
+            self.metadata = json.load(open(self.data_dir+'valid_dict.json'))
         
     def get_raw_image(self, index):
-        return Image.open(self.folder+self.metadata[self.keys[index]]['video_path']).convert("RGB").resize((self.image_size, self.image_size), Image.ANTIALIAS)
+        return Image.open(os.path.join(self.folder, self.metadata[self.keys[index]]['video_path'])).convert("RGB").resize((self.image_size, self.image_size), Image.ANTIALIAS)
 
     def __getitem__(self, index):
         return self.get_suite(index)

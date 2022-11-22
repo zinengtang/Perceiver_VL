e2f1a96e36934c371637daba48895b7e6d5a66b8
Zineng Tang <82334223+zinengtang@users.noreply.github.com>
2022-11-22 01:37:05 -0500

    Update vqa_finetune.sh

--- scripts/vqa_finetune.sh
+++ scripts/vqa_finetune.sh
@@ -1,4 +1,4 @@
 python run.py with data_root='datasets/vqav2' gpus=4 num_nodes=1 task_finetune_vqa \
-per_gpu_batchsize=64 model_type='PerceiverVL' learning_rate=1e-4 load_pretrain=False latent_size_s=128 use_text=True \
-layer_drop=0.5 use_decoder=False num_workers=16 max_epochs=10 \
+per_gpu_batchsize=16 model_type='PerceiverVL' learning_rate=1e-4 load_pretrain=False latent_size_s=128 use_text=True \
+layer_drop=0.5 use_decoder=False num_workers=12 max_epochs=10 \
 load_path=""
